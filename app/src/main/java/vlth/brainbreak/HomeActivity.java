4888bf223d2e2ccf6b0d9d95e0686fa0e1151bfd
nguyentoanthang <nguyentoanthang18@gmail.com>
2015-10-30 00:35:11 +0700

    Update
    + Add new Activity
    * Update HomeActivity
    * Fix tool bar in layout activity_main

--- app/src/main/java/vlth/brainbreak/HomeActivity.java
+++ app/src/main/java/vlth/brainbreak/HomeActivity.java
@@ -34,12 +34,12 @@ import vlth.brainbreak.Util.ID;
 
 public class HomeActivity extends AppCompatActivity {
     public static final String[] titles = new String[]{"Higer or Lower",
-            "Mix Word", "Freaking Math", "Color or Shape"};
+            "Mix Word", "Freaking Math", "Color or Shape", "Find Image"};
 //    public static final String[] titles = new String[] { "Higer or Lower",
 //            "Mix Word", "Freaking Math", "Color or Shape", "Find Image"};
 
 //    public static int[] icon = {R.drawable.hl, R.drawable.wm, R.drawable.fm, R.drawable.geo, R.drawable.find};
-    public static int[] icon = {R.drawable.hl, R.drawable.wm, R.drawable.fm, R.drawable.geo};
+    public static int[] icon = {R.drawable.hl, R.drawable.wm, R.drawable.fm, R.drawable.geo, R.drawable.find};
     ListView listView;
     List<ItemGame> rowItems;
     private HighScore highScore;
@@ -108,7 +108,8 @@ public class HomeActivity extends AppCompatActivity {
         int[] best_score={highScore.getScore(ID.HIGH_SCORE_HIGHER_OR_LOWER,0),
                 highScore.getScore(ID.HIGH_SCORE_MIX_WORD,0),
                 highScore.getScore(ID.HIGH_SCORE_FREAKING_MATH,0),
-                highScore.getScore(ID.HIGH_SCORE_COLOR_SHAPE,0)};
+                highScore.getScore(ID.HIGH_SCORE_COLOR_SHAPE,0),
+                highScore.getScore(ID.HIGH_SCORE_FIND_IMAGE, 0)};
 
         rowItems = new ArrayList<ItemGame>();
         for (int i = 0; i < titles.length; i++) {
@@ -139,8 +140,8 @@ public class HomeActivity extends AppCompatActivity {
                         startActivity(new Intent(HomeActivity.this, ColorShape.class));
                         finish();
                         break;
-//                    case 4:
-//                        startActivity(new Intent(HomeActivity.this, NumberMemory.class));
+                    case 4:
+                        startActivity(new Intent(HomeActivity.this, ImageMemory.class));
                 }
             }
         });
