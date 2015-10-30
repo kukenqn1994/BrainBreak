367d507555fc8aa39befca0e9b4a21f24f6fa5f1
vinhphu458 <vinhphu.458@gmail.com>
2015-10-30 15:23:08 +0700

    test id fb app

--- app/src/main/java/vlth/brainbreak/EndDialog.java
+++ app/src/main/java/vlth/brainbreak/EndDialog.java
@@ -1,33 +1,59 @@
 package vlth.brainbreak;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.Handler;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareDialog;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 import vlth.brainbreak.Util.HighScore;
 import vlth.brainbreak.Util.ID;
 
 public class EndDialog extends Dialog {
 
+    private final ShareDialog shareDialog;
+    private final CallbackManager callbackManager;
     private RelativeLayout root;
     private int current_score = 0;
     private int best_score = 0;
     private TextView mTvYourMove, mTvYourBest;
+    Button share;
 
-    public EndDialog(Context context, final Handler handler) {
+    public EndDialog(final Context context, final Handler handler) {
         super(context);
         // TODO Auto-generated constructor stub
+        FacebookSdk.sdkInitialize(context.getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog((Activity) context);
+
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.end_dialog);
         getWindow().setBackgroundDrawableResource(R.drawable.bg_dialog);
+        RelativeLayout layout=(RelativeLayout)this.findViewById(R.id.root);
 
         mTvYourMove = (TextView) this.findViewById(R.id.yourMove);
         mTvYourBest = (TextView) this.findViewById(R.id.yourBest);
+        share=(Button)findViewById(R.id.btShare);
 
         root = (RelativeLayout) findViewById(R.id.root);
         root.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +69,19 @@ public class EndDialog extends Dialog {
             public void onDismiss(DialogInterface dialog) {
                 // TODO Auto-generated method stub
                 handler.sendEmptyMessage(0);
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharePhoto photo = new SharePhoto.Builder()
+                        .setBitmap(takeScreenshot())
+                        .build();
+                SharePhotoContent content = new SharePhotoContent.Builder()
+                        .addPhoto(photo)
+                        .build();
+                ShareDialog.show((Activity) context, content);
             }
         });
 
@@ -78,5 +117,37 @@ public class EndDialog extends Dialog {
         mTvYourBest.setText("BEST SCORE: " + best_score);
     }
 
-
+    public Bitmap takeScreenshot() {
+        View rootView = findViewById(android.R.id.content).getRootView();
+        rootView.setDrawingCacheEnabled(true);
+        return rootView.getDrawingCache();
+    }
+//
+//    public static void store(Bitmap bm, String fileName){
+//        final String dir = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Screenshots";
+//        File path = new File(dir);
+//        if(!path.exists())
+//            path.mkdirs();
+//        File file = new File(path, fileName);
+//        try {
+//            FileOutputStream fOut = new FileOutputStream(file);
+//            bm.compress(Bitmap.CompressFormat.PNG, 85, fOut);
+//            fOut.flush();
+//            fOut.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private void shareImage(File file){
+//        Uri uri = Uri.fromFile(file);
+//        Intent intent = new Intent();
+//        intent.setAction(Intent.ACTION_SEND);
+//        intent.setType("image/*");
+//
+//        intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "");
+//        intent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+//        intent.putExtra(Intent.EXTRA_STREAM, uri);
+//        getOwnerActivity().startActivity(Intent.createChooser(intent, "Share Screenshot"));
+//    }
 }
--- app/src/main/java/vlth/brainbreak/HomeActivity.java
+++ app/src/main/java/vlth/brainbreak/HomeActivity.java
@@ -1,16 +1,33 @@
 package vlth.brainbreak;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageInstaller;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -19,13 +36,25 @@ import com.facebook.FacebookOperationCanceledException;
 import com.facebook.FacebookSdk;
 import com.facebook.internal.WebDialog;
 import com.facebook.share.Sharer;
+import com.facebook.share.model.AppInviteContent;
 import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.AppInviteDialog;
 import com.facebook.share.widget.ShareButton;
 import com.facebook.share.widget.ShareDialog;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import vlth.brainbreak.Adapter.ListGameAdapter;
 import vlth.brainbreak.Model.ItemGame;
@@ -49,51 +78,46 @@ public class HomeActivity extends AppCompatActivity {
     private Bundle dialogParams = null;
     private CallbackManager callbackManager;
     private ShareDialog shareDialog;
+    LinearLayout ll;
+    private Bitmap myBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
+
         callbackManager = CallbackManager.Factory.create();
         shareDialog = new ShareDialog(this);
         // this part is optional
 //        shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
 //            ...});
         setContentView(R.layout.activity_home);
+
         btInvite = (Button) findViewById(R.id.btInvite);
         btLogin = (Button) findViewById(R.id.btLogin);
         btInvite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                String appLinkUrl, previewImageUrl;
-//
-//                appLinkUrl = "https://fb.me/787851144692434";
-//                previewImageUrl = "http://2.bp.blogspot.com/-99shOruuadw/VQsG2T233sI/AAAAAAAAEi0/noFTxUBh_rg/s1600/appscripts.png";
-//
-//                AppInviteContent content = new AppInviteContent.Builder()
-//                        .setApplinkUrl(appLinkUrl)
-//                        .setPreviewImageUrl(previewImageUrl)
-//                        .build();
-//                AppInviteDialog.show(HomeActivity.this, content);
+                String appLinkUrl, previewImageUrl;
+
+                appLinkUrl = "https://fb.me/787851144692434";
+                previewImageUrl = "http://2.bp.blogspot.com/-99shOruuadw/VQsG2T233sI/AAAAAAAAEi0/noFTxUBh_rg/s1600/appscripts.png";
+
+                AppInviteContent content = new AppInviteContent.Builder()
+                        .setApplinkUrl(appLinkUrl)
+                        .setPreviewImageUrl(previewImageUrl)
+                        .build();
+                AppInviteDialog.show(HomeActivity.this, content);
 
 //                startActivity(new Intent(HomeActivity.this,InviteFriendsActivity.class));
 
-                ShareLinkContent content = new ShareLinkContent.Builder()
-                        .setContentUrl(Uri.parse("https://developers.facebook.com"))
-                        .build();
-                ShareDialog.show(HomeActivity.this, content);
-//                ShareButton shareButton = (ShareButton)findViewById(R.id.fb_share_button);
-//                shareButton.setShareContent(content);
-//                if (ShareDialog.canShow(ShareLinkContent.class)) {
-//                    ShareLinkContent linkContent = new ShareLinkContent.Builder()
-//                            .setContentTitle("Hello Facebook")
-//                            .setContentDescription(
-//                                    "The 'Hello Facebook' sample  showcases simple Facebook integration")
-//                            .setContentUrl(Uri.parse("http://developers.facebook.com/android"))
-//                            .build();
-//
-//                    shareDialog.show(linkContent);
-//                }
+//                SharePhoto photo = new SharePhoto.Builder()
+//                        .setBitmap(myBitmap)
+//                        .build();
+//                SharePhotoContent content = new SharePhotoContent.Builder()
+//                        .addPhoto(photo)
+//                        .build();
+//                ShareDialog.show(HomeActivity.this, content);
 
 
             }
@@ -144,11 +168,64 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
+        ll=(LinearLayout)findViewById(R.id.home_layout);
+        ll.post(new Runnable() {
+            @Override
+            public void run() {
+                myBitmap=captureScreen(ll);
+                Toast.makeText(getApplicationContext(), "Screenshot captured..!", Toast.LENGTH_LONG).show();
+                try {
+
+                    if(myBitmap!=null){
+                        //save image to SD card
+                        saveImage(myBitmap);
+                        Toast.makeText(getApplicationContext(), "Screenshot saved..!", Toast.LENGTH_LONG).show();
+                    }
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
+
 
     }
+    public Bitmap captureScreen(View v) {
+        Display display = ((WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        int width = display.getWidth();
+        int height = display.getHeight();
+        Bitmap screenshot = null;
+        try {
+            if(v!=null) {
+
+                screenshot = Bitmap.createBitmap(width,height, Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(screenshot);
+                v.draw(canvas);
+            }
+
+        }catch (Exception e){
+            Log.d("ScreenShotActivity", "Failed to capture screenshot because:" + e.getMessage());
+        }
+
+        return screenshot;
+    }
+
+    public static void saveImage(Bitmap bitmap) throws IOException {
+
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 40, bytes);
+        File f = new File(Environment.getExternalStorageDirectory() + File.separator + "test.png");
+        f.createNewFile();
+        FileOutputStream fo = new FileOutputStream(f);
+        fo.write(bytes.toByteArray());
+        fo.close();
+    }
+
     @Override
     protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+
 }
